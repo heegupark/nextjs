b480e7d93e4abd0ff4a989b9935c65075b528f41
heegupark <slhead@gmail.com>
2020-11-27 20:48:59 -0800

    removed unused codes

--- pages/[_id]/index.tsx
+++ pages/[_id]/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import IndexPage from '../../components/index-page';
 
 export default function Index() {
@@ -13,19 +12,6 @@ export default function Index() {
   return (
     <>
       <IndexPage />
-      <div
-        style={{
-          backgroundColor: 'rgb(95, 188, 190)',
-          width: '100%',
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <CircularProgress />
-      </div>
     </>
   );
 }
